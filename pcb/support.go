b1da8a661239d4c3c23bde953436461d48068cb6
Octopus Octopus <oct2pus@jade.moe>
2022-12-03 15:25:16 -0600

    take 3 on the support tower

--- pcb/support.go
+++ pcb/support.go
@@ -13,22 +13,22 @@ const (
 	BASE_Y             = 60.0
 	BASE_POST_OFFSET_Y = BASE_Y - POST_OFFSET
 	PROFILE_DISTANCE   = 10.0
-	PILLAR_HEIGHT      = BOARD_Y - 5
+	PILLAR_HEIGHT      = BOARD_Y - 10
 	TRIANGLE_HEIGHT    = BASE_X - POST_OFFSET
+	TRIANGLE_LENGTH    = BASE_POST_OFFSET_Y - INSERT_DIAMETER/2 - POST_OFFSET
 )
 
 // 3D
 
 func pillar() sdf.SDF3 {
-	length := BASE_POST_OFFSET_Y - INSERT_DIAMETER/2 - POST_OFFSET
 	offset := 3.0
 	longInsertLength := 6.35
 
-	bottomTri, err := triangle(length, TRIANGLE_HEIGHT, offset)
+	bottomTri, err := triangle(TRIANGLE_LENGTH, TRIANGLE_HEIGHT, offset)
 	if err != nil {
 		log.Fatal(err)
 	}
-	topTri, err := triangle(length/2, TRIANGLE_HEIGHT/2, offset/4)
+	topTri, err := triangle(TRIANGLE_LENGTH/2, TRIANGLE_HEIGHT/2, offset)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -42,7 +42,7 @@ func pillar() sdf.SDF3 {
 	}
 
 	mount = sdf.Transform3D(mount, sdf.RotateX(sdf.DtoR(90)))
-	mount = sdf.Transform3D(mount, sdf.Translate3d(v3.Vec{X: 0, Y: -TRIANGLE_HEIGHT/2 - (-longInsertLength / 2), Z: -PILLAR_HEIGHT/2 + POST_Y + PROFILE_DISTANCE/2 - INSERT_HEIGHT}))
+	mount = sdf.Transform3D(mount, sdf.Translate3d(v3.Vec{X: 0, Y: -TRIANGLE_HEIGHT/2 - longInsertLength/2 + longInsertLength, Z: -PILLAR_HEIGHT/2 - INSERT_HEIGHT + POST_Y}))
 
 	return sdf.Difference3D(pillar, mount)
 }
@@ -56,7 +56,7 @@ func support() sdf.SDF3 {
 
 	base = sdf.Union3D(
 		base,
-		sdf.Transform3D(pillar, sdf.Translate3d(v3.Vec{X: 0, Y: 0, Z: INSERT_HEIGHT/2 + PILLAR_HEIGHT/2})),
+		sdf.Transform3D(pillar, sdf.Translate3d(v3.Vec{X: TRIANGLE_LENGTH / 48, Y: 0, Z: INSERT_HEIGHT/2 + PILLAR_HEIGHT/2})),
 	)
 	return base
 }
@@ -93,12 +93,12 @@ func base2D() sdf.SDF2 {
 func triangle(length, height, offset float64) (sdf.SDF2, error) {
 	x, y := length-offset, height-offset
 	dimens := []v2.Vec{ // left to top to right
-		{X: 0, Y: -offset},
-		{X: 0, Y: offset},
+		{X: offset / 2, Y: -offset / 2},
+		{X: -offset / 2, Y: offset / 2},
 		{X: x/2 - offset, Y: y},
 		{X: x/2 + offset, Y: y},
-		{X: x, Y: offset},
-		{X: x, Y: -offset},
+		{X: x + offset/2, Y: offset - offset/2},
+		{X: x - offset/2, Y: -offset + offset/2},
 	}
 	poly, err := sdf.Polygon2D(dimens)
 	return sdf.Center2D(poly), err
